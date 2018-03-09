4a23c45dc8bb7b3b05c56aa79d3336f72174d2ce
João Neto <jneto@fe.up.pt>
2018-03-09 19:55:46 +0000

    Coordinate Conversion using ProjNet
    Signed-off-by: João Neto <jneto@fe.up.pt>

--- Assets/ElevationAPI/GeoWorld.cs
+++ Assets/ElevationAPI/GeoWorld.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿using ProjNet.CoordinateSystems;
+using ProjNet.CoordinateSystems.Transformations;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -27,6 +29,20 @@ public class GeoWorld : MonoBehaviour
         ProcessArea();
 
         StartCoroutine(APICommunication.GridElevationRequest(new Rect(xMin, yMin, xMax - xMin, yMax - yMin), gridSize.x));
+
+        int zone = GetZone(p1.x, p1.y);
+        string band = GetBand(p1.x);
+        //Transform to UTM
+        CoordinateTransformationFactory ctfac = new CoordinateTransformationFactory();
+        ICoordinateSystem wgs84geo = ProjNet.CoordinateSystems.GeographicCoordinateSystem.WGS84;
+        ICoordinateSystem utm = ProjNet.CoordinateSystems.ProjectedCoordinateSystem.WGS84_UTM(zone, p1.x > 0);
+        ICoordinateTransformation trans = ctfac.CreateFromCoordinateSystems(wgs84geo, utm);
+        double[] pUtm = trans.MathTransform.Transform(new double[] { p1.y, p1.x });
+
+        double easting = pUtm[0];
+        double northing = pUtm[1];
+
+        Debug.Log(zone + " " + band + " " + easting + " " + northing);
     }
 
 
@@ -81,11 +97,78 @@ public class GeoWorld : MonoBehaviour
             }
         }
 
-        MeshData meshData = MeshGeneration.GenerateMeshFromHeigthMap(heightMap, 1);
+        MeshData meshData = MeshGeneration.GenerateMeshFromHeigthMap(heightMap, .05f);
 
         GetComponent<MeshFilter>().mesh = meshData.CreateMesh();
 
         Debug.Log("Populated GeoData");
+    }
+
+    private static int GetZone(double latitude, double longitude)
+    {
+        // Norway
+        if (latitude >= 56 && latitude < 64 && longitude >= 3 && longitude < 13)
+            return 32;
+
+        // Spitsbergen
+        if (latitude >= 72 && latitude < 84)
+        {
+            if (longitude >= 0 && longitude < 9)
+                return 31;
+            else if (longitude >= 9 && longitude < 21)
+                return 33;
+            if (longitude >= 21 && longitude < 33)
+                return 35;
+            if (longitude >= 33 && longitude < 42)
+                return 37;
+        }
+
+        return (int)Mathf.Ceil(((float)longitude + 180f) / 6f);
+    }
+    private static string GetBand(double latitude)
+    {
+        if (latitude <= 84 && latitude >= 72)
+            return "X";
+        else if (latitude < 72 && latitude >= 64)
+            return "W";
+        else if (latitude < 64 && latitude >= 56)
+            return "V";
+        else if (latitude < 56 && latitude >= 48)
+            return "U";
+        else if (latitude < 48 && latitude >= 40)
+            return "T";
+        else if (latitude < 40 && latitude >= 32)
+            return "S";
+        else if (latitude < 32 && latitude >= 24)
+            return "R";
+        else if (latitude < 24 && latitude >= 16)
+            return "Q";
+        else if (latitude < 16 && latitude >= 8)
+            return "P";
+        else if (latitude < 8 && latitude >= 0)
+            return "N";
+        else if (latitude < 0 && latitude >= -8)
+            return "M";
+        else if (latitude < -8 && latitude >= -16)
+            return "L";
+        else if (latitude < -16 && latitude >= -24)
+            return "K";
+        else if (latitude < -24 && latitude >= -32)
+            return "J";
+        else if (latitude < -32 && latitude >= -40)
+            return "H";
+        else if (latitude < -40 && latitude >= -48)
+            return "G";
+        else if (latitude < -48 && latitude >= -56)
+            return "F";
+        else if (latitude < -56 && latitude >= -64)
+            return "E";
+        else if (latitude < -64 && latitude >= -72)
+            return "D";
+        else if (latitude < -72 && latitude >= -80)
+            return "C";
+        else
+            return null;
     }
 }
 
