8408eba4621f08cef4a9ff341430a0b4d94eb820
João Neto <jneto@fe.up.pt>
2018-03-09 15:12:48 +0000

    Generating HeightMap and Mesh. Needs scaling
    Signed-off-by: João Neto <jneto@fe.up.pt>

--- Assets/ElevationAPI/APICommunication.cs
+++ Assets/ElevationAPI/APICommunication.cs
@@ -1,15 +1,23 @@
-﻿using System.Collections;
+﻿using Newtonsoft.Json;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
 public class APICommunication : MonoBehaviour {
-    
+
+    static string elevationPath = "https://maps.googleapis.com/maps/api/elevation/";
+    static string apiKey = "AIzaSyAl1Bz8x72uVaARqPtVwF0rvB2STkNI7cc";
+
+    public Vector2 fromCoords;
+    public Vector2 toCoords;
+    public int samples;
 
     // Use this for initialization
     void Start ()
     {
-        StartCoroutine(GetText());
+        //StartCoroutine(LineElevationRequest(fromCoords, toCoords, samples));
+
 
     }
 	
@@ -18,24 +26,97 @@ public class APICommunication : MonoBehaviour {
 		
 	}
 
-    IEnumerator GetText()
+    public static ElevationResponse LineElevationRequest(Vector2 from, Vector2 to, int nSamples)
     {
-        UnityWebRequest www = UnityWebRequest.Get("https://maps.googleapis.com/maps/api/elevation/json?locations=40.714728,-73.998672|-34.397,150.644&key=AIzaSyAl1Bz8x72uVaARqPtVwF0rvB2STkNI7cc");
-        
+        //https://maps.googleapis.com/maps/api/elevation/json?path=36.578581,-118.291994|36.23998,-116.83171&samples=3&key=AIzaSyAl1Bz8x72uVaARqPtVwF0rvB2STkNI7cc
 
-        yield return www.Send();
+        string requestPath = elevationPath + "json?path=" + from.x + "," + from.y + "|"+ to.x + "," + to.y + "&samples=" + nSamples + "&key=" + apiKey;
 
-        if (www.isError)
+        UnityWebRequest request = UnityWebRequest.Get(requestPath);
+
+        if(request.isNetworkError)
         {
-            Debug.Log(www.error);
+            Debug.LogError(request.error);
         }
         else
         {
-            // Show results as text
-            Debug.Log(www.downloadHandler.text);
+            Debug.Log(request.downloadHandler.text);
+            ElevationResponse response = JsonConvert.DeserializeObject<ElevationResponse>(request.downloadHandler.text);
 
-            // Or retrieve results as binary data
-            byte[] results = www.downloadHandler.data;
+            if(response.status == "OK")
+            {
+                foreach (Result r in response.results)
+                {
+                    Debug.Log("(" + r.location.lat + "," + r.location.lng + ") -> " + r.elevation);
+                }
+            }
+            return response;
         }
+        
+
+        return null;
+        
     }
+
+    public static IEnumerator GridElevationRequest(Rect area, int gridRes)
+    {
+        List<Vector2> samplePoints = new List<Vector2>();
+
+        float areaWidth = area.xMax - area.xMin;
+        float areaHeight = area.yMax - area.yMin;
+
+
+        for (float y = area.yMin; y < area.yMax; y+=(areaHeight/gridRes))
+        {
+            for (float x = area.xMin; x < area.xMax; x += (areaWidth / gridRes))
+            {
+                //float sampleX = area.xMin + x * areaWidth / gridRes;
+                //float sampleY = area.yMin + y * areaHeight / gridRes;
+                samplePoints.Add(new Vector2(x,y));
+            }
+        }
+
+        string requestPath = elevationPath + "json?locations=";
+
+        for (int pIndex = 0; pIndex < samplePoints.Count; pIndex++)
+        {
+            requestPath += samplePoints[pIndex].x + "," + samplePoints[pIndex].y;
+            if (pIndex != samplePoints.Count-1)
+            {
+                requestPath += "|";
+            }
+        }
+
+        requestPath += "&key=" + apiKey;
+
+        Debug.Log(requestPath);
+
+        UnityWebRequest request = UnityWebRequest.Get(requestPath);
+        yield return request.SendWebRequest();
+
+        if (request.isNetworkError)
+        {
+            Debug.LogError(request.error);
+        }
+        else
+        {
+            Debug.Log(request.downloadHandler.text);
+            ElevationResponse response = JsonConvert.DeserializeObject<ElevationResponse>(request.downloadHandler.text);
+
+            if (response.status == "OK")
+            {
+                foreach (Result r in response.results)
+                {
+                    Debug.Log("(" + r.location.lat + "," + r.location.lng + ") -> " + r.elevation);
+                }
+
+                FindObjectOfType<GeoWorld>().onElevationRequestComplete(response);
+            }
+        }
+
+        yield return null;
+    }
+
+
+    
 }
---
+++ Assets/ElevationAPI/GeoWorld.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeoWorld : MonoBehaviour
+{
+
+    public Vector2Int gridSize;
+
+    public Vector2 centerPoint;
+    public Vector2 areaSize;
+
+    public Vector2 p1;
+    public Vector2 p2;
+
+
+    public Result[,] geoData;
+    float[,] heightMap;
+
+    float xMin, yMin, xMax, yMax;
+
+    // Use this for initialization
+    void Start()
+    {
+        geoData = new Result[gridSize.x, gridSize.y];
+
+        ProcessArea();
+
+        StartCoroutine(APICommunication.GridElevationRequest(new Rect(xMin, yMin, xMax - xMin, yMax - yMin), gridSize.x));
+    }
+
+
+    void ProcessArea()
+    {
+        if (p1.x <= p2.x)
+        {
+            xMin = p1.x;
+            xMax = p2.x;
+        }
+        else
+        {
+            xMin = p2.x;
+            xMax = p1.x;
+        }
+
+        if (p1.y <= p2.y)
+        {
+            yMin = p1.y;
+            yMax = p2.y;
+        }
+        else
+        {
+            yMin = p2.y;
+            yMax = p1.y;
+        }
+    }
+
+    public void onElevationRequestComplete(ElevationResponse response)
+    {
+        double lowestAltitude = double.MaxValue;
+
+        for (int y = 0; y < gridSize.y; y++)
+        {
+            for (int x = 0; x < gridSize.x; x++)
+            {
+                geoData[x, y] = response.results[y * gridSize.x + x];
+                if (geoData[x, y].elevation < lowestAltitude)
+                {
+                    lowestAltitude = geoData[x, y].elevation;
+                }
+            }
+        }
+
+        heightMap = new float[gridSize.x, gridSize.y];
+
+        for (int y = 0; y < gridSize.y; y++)
+        {
+            for (int x = 0; x < gridSize.x; x++)
+            {
+                heightMap[x, y] = (float)(geoData[x, y].elevation - lowestAltitude);
+            }
+        }
+
+        MeshData meshData = MeshGeneration.GenerateMeshFromHeigthMap(heightMap, 1);
+
+        GetComponent<MeshFilter>().mesh = meshData.CreateMesh();
+
+        Debug.Log("Populated GeoData");
+    }
+}
+
+
+
+public class Location
+{
+    public double lat { get; set; }
+    public double lng { get; set; }
+}
+
+public class Result
+{
+    public double elevation { get; set; }
+    public Location location { get; set; }
+    public double resolution { get; set; }
+}
+
+public class ElevationResponse
+{
+    public List<Result> results { get; set; }
+    public string status { get; set; }
+}
